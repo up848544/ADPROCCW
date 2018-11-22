788b9035f209ce67a3af19fbe2663ab354bfff8b
Albert <no-reply>
2018-11-22 14:48:35 +0000

    Restructure Box Class
    Rewrote Box Class

--- FlexBox/src/flexbox/Box.java
+++ FlexBox/src/flexbox/Box.java
@@ -12,10 +12,34 @@ package flexbox;
 public class Box
 {
     int grade, quantity;
-    double depth, height, width, price, base;
+    double depth, height, width, base, modifier;
     boolean sealableTop;
     double[] gradePrice = {0.55, 0.65, 0.82, 0.98, 1.5};
+    
+    //Type 1
     public Box(double height, double width, double depth, int grade, boolean sealableTop, int quantity)
+    {
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
+        this.grade = grade;
+        this.sealableTop = sealableTop;
+        this.quantity = quantity;
+    }
+    
+    //Type 2
+    public Box(double height, double width, double depth, int grade, int colour, boolean sealableTop, int quantity)
+    {
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
+        this.grade = grade;
+        this.sealableTop = sealableTop;
+        this.quantity = quantity;
+    }
+    
+    //Type 3, 4 and 5
+    public Box(double height, double width, double depth, int grade, int colour, boolean reinforcedBottom, boolean reinforcedCorner, boolean sealableTop, int quantity)
     {
         this.depth = depth;
         this.height = height;
@@ -27,25 +51,27 @@ public class Box
     
     public double area()
     {
-        return (this.height * this.width * this.depth);
+        return (2*(height*width) + 2*(height*depth) + 4*(width*depth));
     }
     
-    public double base_price()
+    public double base_price(int g)
     {
-        this.price = this.base = area() * gradePrice[this.grade - 1];
-        return this.base;
+        return this.base = gradePrice[g-1];
     }
     
     public double box_price()
     {
-        base_price();
-        if(sealableTop)this.price += this.base*0.1;
-        return this.price;
+        return (getQuantity() * area() * (base_price(grade) * modifier));
     }
     
     public int getQuantity()
     {
         return this.quantity;
+    }
+    
+    public void hasSealableTop(boolean top)
+    {
+        if(top)modifier+=0.1;
     }
     
 }
