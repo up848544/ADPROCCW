a0f0bde25b8b48e73f2d4316de6f3bcdbeae3e8b
Albert <no-reply>
2018-11-26 22:13:40 +0000

    Added GUI and some functionality too it

--- FlexBox/src/flexbox/Order.java
+++ FlexBox/src/flexbox/Order.java
@@ -22,78 +22,17 @@ public class Order
         this.boxes_Ordered = new ArrayList<>();
     }
     
-    public void addBox(Box newBox)
+    public boolean canSupply(double h, double w, double d, int g, int col, boolean t, boolean b, boolean c, int q)
     {
-        boxes_Ordered.add(newBox);
+        if(g<=3&&col==0&&!b&&!c)return true;
+        if(g>=2&&g<=4&&col==1&&!b&&!c)return true;
+        if(g>=2&&col==2&&!b&!c)return true;
+        if(g>=2&&col==2&&b&&!c)return true;
+        if(g>=3&&col==2&b&c)return true;
+        return false;
     }
-    
-    public void removeBox(Box box)
+    public void addBox(double h, double w, double d, int g, int col, boolean t, boolean b, boolean c, int q)
     {
-        boxes_Ordered.remove(box);
-    }
-    
-    public ArrayList<Box> getBoxes()
-    {
-        return boxes_Ordered;
-    }
-    
-    public double getTotalPrice()
-    {
-        this.totalPrice = 0;
-        for(Box b : boxes_Ordered)
-        {
-            this.totalPrice += b.box_price() * b.getQuantity();
-        }
-        return this.totalPrice;
-    }
-    
-    public void setHeight(double h)
-    {
-        this.config[0] = h;
-    }
-    public void setWidth(double w)
-    {
-        this.config[1] = w;
-    }
-    public void setDepth(double d)
-    {
-        this.config[2] = d;
-    }
-    public void setGrade(int g)
-    {
-        this.config[3] = g;
-    }
-    public void setColours(int c)
-    {
-        this.config[4] = c;
-    }
-    public void setTop(int t)
-    {
-        this.config[5] = t;
-    }
-    public void setBottom(int b)
-    {
-        this.config[6] = b;
-    }
-    public void setCorners(int c)
-    {
-        this.config[7] = c;
-    }
-    public void setQuantity(int q)
-    {
-        this.config[8] = q;
-    }
-    public boolean checkFullfillment()
-    {
-        boolean canBeFullfilled = true;
-        for(int i = 0; i<3; i++)
-        {
-            if(config[i] < 0.1 || config[i] > 5)
-            {
-                canBeFullfilled = false;
-            }
-        }
-        if(config[8] > 100)canBeFullfilled=false;
-        return canBeFullfilled;
+        
     }
 }
